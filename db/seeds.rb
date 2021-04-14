d32b8f9c6cca7eef0901d28f0e56fa3e87c9f7f8
johndannug <johndannug17@gmail.com>
2021-04-14 15:21:30 +0800

    Implemented custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )
+end
+
 10.times do |blog|
     Blog.create!(
         title: "My Blog Post #{blog}",
@@ -20,7 +26,8 @@
                 aliquid ex ea commodi consequatur? Quis autem 
                 vel eum iure reprehenderit qui in ea voluptate 
                 velit esse quam nihil molestiae consequatur, 
-                vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+                vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                topic_id: Topic.last.id
     )
 end
 
@@ -37,10 +44,31 @@ puts "5 skills created"
 
 
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
     Portfolio.create!(
         title: "Portfolio title: #{portfolio_item}",
-        subtitle: "My great service",
+        subtitle: "Ruby on Rails",
+        body: "Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut 
+                labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis 
+                nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit 
+                in voluptate velit esse cillum dolore 
+                eu fugiat nulla pariatur. Excepteur sint 
+                occaecat cupidatat non proident, sunt 
+                in culpa qui officia deserunt mollit anim id est laborum.",
+        main_image: "http://placehold.it/600x400",
+        thumb_image: "http://placehold.it/350x200"
+    )
+end
+
+1.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "Angular",
         body: "Lorem ipsum dolor sit amet, 
                 consectetur adipiscing elit, 
                 sed do eiusmod tempor incididunt ut 
